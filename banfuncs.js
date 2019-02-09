d205e98e3376a9d6f5cb46a355e0ad962846e3e8
coolxpress <44113395+ServerRelay@users.noreply.github.com>
2019-02-09 11:32:39 -0400

    Add files via upload

---
+++ banfuncs.js
@@ -0,0 +1,18 @@
+const pg=require('pg')
+const Discord=require('discord.js')
+
+
+module.exports.CSRBan=async function (client,db,usr){
+            await db.query(`INSERT OR REPLACE INTO banned (id) VALUES (${usr.id})`)
+            client.banlist.push(usr.id)
+}
+
+/**
+ * @param {sqlite.DB} db database
+ * @param {Discord.User} usr user
+ */
+module.exports.CSRUnban=async function (client,db,usr){
+    await db.query(`DELETE FROM banned WHERE id = ${usr.id}`)
+    client.banlist.splice(client.banlist.findIndex(x=>x==usr.id),1)
+
+}

1b9aa0f3a4fd184da477deb4cc84c3fc52d0ed1a
DeltaRade <46230290+DeltaRade@users.noreply.github.com>
2019-02-14 17:14:03 -0400

    Add files via upload

---
+++ commands/lockdown.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+    name: 'lockdown',
+    description: 'sends the rules to using the #irc channel',
+    execute(message) {
+        const {staff}=require(`./commands/stafflist.json`)  
+        if(!staff.includes(message.author.id)){ 
+            return message.channel.send('you dont have access to that command')
+        }
+        if(message.client.lockdown){
+            message.client.lockdown=false
+            message.channel.send('lockdown ended')
+        }
+        else{
+            message.client.lockdown=true
+            message.channel.send('lockdown has begun')
+        }
+    },
+};

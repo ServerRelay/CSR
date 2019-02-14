373142ce0b0c8fc273af5ddcfc65945de0c405ee
DeltaRade <46230290+DeltaRade@users.noreply.github.com>
2019-02-14 17:38:41 -0400

    Update lockdown.js

--- commands/lockdown.js
+++ commands/lockdown.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'lockdown',
     description: 'sends the rules to using the #irc channel',
     execute(message) {
-        const {staff}=require(`./commands/stafflist.json`)  
+        const {staff}=require(`./stafflist.json`)  
         if(!staff.includes(message.author.id)){ 
             return message.channel.send('you dont have access to that command')
         }

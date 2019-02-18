d6a984a4ffbb578d8eae35731c96f9dfde617807
DeltaRade <coolxpress26@gmail.com>
2019-02-18 19:54:17 -0400

    allow for staff

--- commands/leave.js
+++ commands/leave.js
@@ -1,8 +1,10 @@
+const { staff } = require('./stafflist.json');
 module.exports = {
 	name: 'leave',
 	execute(message, args) {
-		if(message.author.id !== '298258003470319616') {
-			return message.send('nope');
+		if(staff.findIndex(x=>x === message.author.id) == -1) {
+			message.channel.send('no permission');
+			return;
 		}
 		const guild = message.client.guilds.get(args[0]) || message.client.guilds.find(x=>x.name.toLowerCase().indexOf(args.join(' ').toLowerCase()) != -1);
 		if(guild) {

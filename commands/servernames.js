ae0e381f827d0e39b9d8004491634b98cf7990bf
DeltaRade <coolxpress26@gmail.com>
2019-02-25 17:40:01 -0400

    Create servernames.js

---
+++ commands/servernames.js
@@ -0,0 +1,21 @@
+module.exports = {
+	name: 'servernames',
+	alias:['snames'],
+	staff: 'fetch all server names(delta only)',
+	/**
+     *
+     * @param {Discord.Message} message
+     * @param {[]} args
+     */
+	async execute(message, args) {
+		let text;
+		if(message.author.id != '298258003470319616') {
+			return;
+		}
+
+		message.client.guilds.forEach((guild)=>{
+			text += guild.name;
+		});
+		message.author.send(text, { split:true });
+	},
+};

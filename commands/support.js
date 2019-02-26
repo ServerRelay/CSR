803e20b9236f580be3a3cdccdc1631dc50ed2544
DeltaRade <coolxpress26@gmail.com>
2019-02-26 15:30:25 -0400

    typefix

--- commands/support.js
+++ commands/support.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: 'support',
-	description: 'sends a invite link to join teh official server',
+	description: 'sends a invite link to join the official server',
 	execute(message) {
 		message.author.send('join the support server https://discord.gg/mkjbFXF');
 	},

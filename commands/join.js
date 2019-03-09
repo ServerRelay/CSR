20315e8da07cc84f004b719d94c5fbf8efe31124
PLASMAchicken <plasmachickenmc@gmail.com>
2019-03-09 15:51:29 +0100

    Better Error Handling

--- commands/join.js
+++ commands/join.js
@@ -31,64 +31,81 @@ module.exports = {
 		const ch = sv.channels.find(x=>x.name === 'irc');
 		if (ch) {
 			message.author.send(`awaiting aproval from ${sv.name}...`);
-			const rq = await sv.createChannel('irc request', 'text');
-			await rq.overwritePermissions(sv.id, { 'VIEW_CHANNEL':false });
-			sv.roles.forEach(async role => {
-				if(!role.hasPermission('KICK_MEMBERS')) {return;}
-				await rq.overwritePermissions(role.id, { 'VIEW_CHANNEL':true })
-					.catch(()=>{
-						console.log('error near adding overwrites');
-					});
+			const rq = await sv.createChannel('irc request', 'text', [{
+				id: sv.id,
+				deny: ['MANAGE_MESSAGES'],
+				allow: ['SEND_MESSAGES'],
+			},
+			{
+				id: message.client.user.id,
+				deny: [],
+				allow: 805829713,
+			}]
+			).catch(() => {
+				sv.owner.send(`Somebody tried to join ${sv.name} server but i could not create a #irc-requests channel!`).catch(()=>{
+					ch.send('Somebody tried to join this server but i could not create a #irc-requests channel!');
+				});
 			});
+			try{
+				await rq.overwritePermissions(sv.id, { 'VIEW_CHANNEL':false });
+				sv.roles.forEach(async role => {
+					if(!role.hasPermission('KICK_MEMBERS')) {return;}
+					await rq.overwritePermissions(role.id, { 'VIEW_CHANNEL':true })
+				});
 
-			ed.setAuthor(`user ${message.author.username} is requesting an invite`, message.author.avatarURL || message.author.defaultAvatarURL);
-			ed.addField('Tag', `${message.author.tag}`, false);
-			ed.addField('server:', sv.name, false);
-			// ed.addField('role in server',message.author.roles.first().name)
-			const perm = await rq.send(ed);
-			await perm.react('✅');
-			await perm.react('❎');
-			const filter = (reaction, user)=>(reaction.emoji.name === '✅' || reaction.emoji.name === '❎') && !user.bot;
-			const cll = perm.createReactionCollector(filter);// {time:60000}
+				ed.setAuthor(`user ${message.author.username} is requesting an invite`, message.author.avatarURL || message.author.defaultAvatarURL);
+				ed.addField('Tag', `${message.author.tag}`, false);
+				ed.addField('server:', sv.name, false);
+				// ed.addField('role in server',message.author.roles.first().name)
+				const perm = await rq.send(ed);
+				await perm.react('✅');
+				await perm.react('❎');
 
-			// console.log(kl)
-			// let filter=(reaction,user)=>(reaction.emoji.name==='✅'||reaction.emoji.name==='❎')&&!user.bot
-			// let cll=kl.createReactionCollector(filter,{time:20000})
+				const filter = (reaction, user)=>(reaction.emoji.name === '✅' || reaction.emoji.name === '❎') && !user.bot;
+				const cll = perm.createReactionCollector(filter);// {time:60000}
 
-			cll.on('collect', (rec)=>{
-				if(rec.emoji.name == '❎') {
+
+				// console.log(kl)
+				// let filter=(reaction,user)=>(reaction.emoji.name==='✅'||reaction.emoji.name==='❎')&&!user.bot
+				// let cll=kl.createReactionCollector(filter,{time:20000})
+
+				cll.on('collect', (rec)=>{
+					if(rec.emoji.name == '❎') {
 					// sv.owner.send('sending refusal')
-					message.author.send('denied permission');
-					rq.delete();
-					cll.stop();
-				}
-				if(rec.emoji.name == '✅') {
+						message.author.send('denied permission');
+						rq.delete();
+						cll.stop();
+					}
+					if(rec.emoji.name == '✅') {
 
-					ch.createInvite('someone requested to join this server')
-						.then((invite)=>{
-							message.author.send(`${sv.name}'s invite code:${invite.url}`);
+						ch.createInvite('someone requested to join this server')
+							.then((invite)=>{
+								message.author.send(`${sv.name}'s invite code:${invite.url}`);
 
-						})
-						.catch((err)=>{
-							console.log(err);
-						});
-					rq.delete();
-					cll.stop();
-				}
+							})
+							.catch((err)=>{
+								console.log(err);
+							});
+						rq.delete();
+						cll.stop();
+					}
 
-			});
-			// sv.owner.send(RichEmbed=ed)
-			cll.on('end', (t)=>{
-				if(!t.size > 0) {
+				});
+				// sv.owner.send(RichEmbed=ed)
+				cll.on('end', (t)=>{
+					if(!t.size > 0) {
 
-					message.author.send('no response try again later');
-					rq.delete();
-				}
-			});
+						message.author.send('no response try again later');
+						rq.delete();
+					}
+				});
 
-
+			}
+			catch(err) {
+				message.channel.send('While trying to join the Server an error occured!');
+				rq.send('Somebody tried to join your server, but something went wrong!');
+			}
 		}
-
 
 	},
 };

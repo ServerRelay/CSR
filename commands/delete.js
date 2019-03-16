1cb7aa09841d8b953456195c085115fb5e8d6b24
PLASMAchicken <plasmachickenmc@gmail.com>
2019-03-16 18:27:45 +0100

    Update delete.js

--- commands/delete.js
+++ commands/delete.js
@@ -22,7 +22,16 @@ module.exports = {
 
 		message.client.csrchannels.forEach(async (ch) => {
 			let i = 0;
-			setTimeout(deleteMSG(ch, args[0]), i * 5000);
+			setTimeout(async function deleteMSG() {
+				try{
+					if(ch.permissionsFor(ch.guild.me).has('MANAGE_MESSAGES')) {ch.bulkDelete(args[0], true);}
+					else {ch.send('COULD NOT DELETE LAST MESSAGES BECAUSE I DO NOT HAVE PERMS!');}
+				}
+				catch(e) {
+					console.log(e.name + '[]' + e.message);
+				}
+
+			}, i * 5000);
 			i++;
 
 		});
@@ -30,13 +39,3 @@ module.exports = {
 
 };
 
-async function deleteMSG(ch, num) {
-	try{
-		if(ch.permissionsFor(ch.guild.me).has('MANAGE_MESSAGES')) {ch.bulkDelete(num, true);}
-		else {ch.send('COULD NOT DELETE LAST MESSAGES BECAUSE I DO NOT HAVE PERMS!');}
-	}
-	catch(e) {
-		console.log(e.name + '[]' + e.message);
-	}
-
-}

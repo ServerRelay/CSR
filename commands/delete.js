e7e9b52f307ddcbbd948c236469aa827b9e5c8b9
PLASMAchicken <plasmachickenmc@gmail.com>
2019-03-16 18:03:38 +0100

    Create delete.js

---
+++ commands/delete.js
@@ -0,0 +1,34 @@
+const { staff } = require('./stafflist.json');
+// eslint-disable-next-line no-unused-vars
+const { Message } = require('discord.js');
+
+module.exports = {
+	name: 'delete',
+	/**
+     *
+     *
+     * @param {Message} message
+     * @param {*} args
+     * @returns
+     */
+	async execute(message, args) {
+		if(!args[0] || isNaN(args[0]) || args[0] > 50 || args[0] < 0) args[5] = 5;
+
+
+		if(staff.findIndex(x=>x === message.author.id) == -1) {
+			message.channel.send('no permission');
+			return;
+		}
+
+		message.client.csrchannels.forEach(async (ch) => {
+			try{
+				if(message.channel.permissionsFor(message.channel.guild.me).has('MANAGE_MESSAGES')) {ch.bulkDelete(args[0]);}
+				else {ch.send('COULD NOT DELETE LAST MESSAGES BECAUSE I DO NOT HAVE PERMS!');}
+			}
+			catch(e) {
+				console.log(e.name + '[]' + e.message);
+			}
+		});
+	},
+
+};

71bafe42b37957f0c8e5598d868c499bfcc4a039
PLASMAchicken <plasmachickenmc@gmail.com>
2019-03-16 19:48:31 +0100

    Revert "lets try discord://"
    This reverts commit 4a74a8dea746262d964264466289468438ffb884.

--- irc.js
+++ irc.js
@@ -326,7 +326,7 @@ function sendPrivate(message) {
 
 	const ed = new Discord.RichEmbed()
 		.setColor()
-		.setAuthor(`${message.author.username}`, (message.author.avatarURL || message.author.defaultAvatarURL), `discord:///users/298258003470319616/${message.author.id}`)
+		.setAuthor(`${message.author.username}`, (message.author.avatarURL || message.author.defaultAvatarURL), `https://discordapp.com/users/${message.author.id}`)
 		.setDescription(message.cleanContent)
 		.setTimestamp(new Date())
 		.setFooter(message.guild.name, (message.guild.iconURL || client.user.defaultAvatarURL));

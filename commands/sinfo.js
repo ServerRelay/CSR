17d675f6a42fc5e10b6c27b2ab853f8a0f4d342a
DeltaRade <coolxpress26@gmail.com>
2019-03-18 16:54:12 -0400

    remove useless function

--- commands/sinfo.js
+++ commands/sinfo.js
@@ -1,14 +1,5 @@
 const Discord = require('discord.js');
 const { staff } = require('./stafflist.json');
-function rgbToHex(R, G, B) {return toHex(R) + toHex(G) + toHex(B);}
-
-function toHex(n) {
-	n = parseInt(n, 10);
-	if (isNaN(n)) return '00';
-	n = Math.max(0, Math.min(n, 255));
-	return '0123456789ABCDEF'.charAt((n - n % 16) / 16)
-         + '0123456789ABCDEF'.charAt(n % 16);
-}
 
 module.exports = {
 	name: 'sinfo',
@@ -17,7 +8,7 @@ module.exports = {
 	execute(message, args) {
 		if(staff.findIndex(x=>x === message.author.id) !== -1) {
 			const d = new Discord.RichEmbed()
-				.setColor(rgbToHex(0, 200, 200));
+				.setColor([0, 200, 200]);
 			const sv = message.client.guilds.find(x=>x.name.toLowerCase().indexOf(args.join(' ').toLowerCase()) != -1);
 			d.setAuthor('information about server: ' + sv.name, sv.iconURL);
 			if(sv) {
--- commands/uinfo.js
+++ commands/uinfo.js
@@ -1,15 +1,5 @@
 const Discord = require('discord.js');
 const { staff } = require('./stafflist.json');
-function rgbToHex(R, G, B) {return toHex(R) + toHex(G) + toHex(B);}
-
-function toHex(n) {
-	n = parseInt(n, 10);
-	if (isNaN(n)) return '00';
-	n = Math.max(0, Math.min(n, 255));
-	return '0123456789ABCDEF'.charAt((n - n % 16) / 16)
-         + '0123456789ABCDEF'.charAt(n % 16);
-}
-
 module.exports = {
 	name: 'uinfo',
 	alias:['userinfo'],
@@ -17,7 +7,7 @@ module.exports = {
 	async execute(message, args) {
 		if(staff.findIndex(x=>x === message.author.id) !== -1) {
 			const d = new Discord.RichEmbed()
-				.setColor(rgbToHex(0, 200, 200));
+				.setColor([0, 200, 200]);
 
 			let svn = '';
 			const user = message.mentions.users.first() || message.client.users.get(args[0]) || message.client.users.find(x=>x.tag == args.join(' ')) || message.client.users.find(x=>x.username == args.join(' '));

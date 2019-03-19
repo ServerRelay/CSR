b2e7a28bd772104a77daecba1f2b8109a80b5931
DeltaRade <coolxpress26@gmail.com>
2019-03-19 14:43:53 -0400

    added eval for (possibly) helpful purposes

---
+++ commands/eval.js
@@ -0,0 +1,22 @@
+const cblockre = /(^```js)|(```$)/g;
+module.exports = {
+	name:'eval',
+	alias:['e'],
+	async execute(message, args) {
+		if(message.author.id != message.client.ownerID) {return message.reply('not allowed to use this command');}
+		try {
+			let content = args.join(' ');
+			if (cblockre.test(content)) {
+				content = content.replace(cblockre, '').trim();
+			}
+			let evaled = eval(content);
+			if (typeof evaled !== 'string') {evaled = require('util').inspect(evaled);}
+			const wrapped = `${message.author}\n\`\`\`js\n${evaled.length > 1800 ? evaled.slice(0, 1800) + '\n...' : evaled}\n\`\`\``;
+			await message.channel.send(wrapped);
+			console.log(evaled);
+		}
+		catch (err) {
+			message.channel.send(`\`ERROR\` \`\`\`xl\n${err}\n\`\`\``);
+		}
+	},
+};

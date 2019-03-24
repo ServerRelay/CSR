1761fd3ab04ba68d9ef3fa777732c6938e5ae795
DeltaRade <coolxpress26@gmail.com>
2019-03-24 16:39:11 -0400

    Update staffjoin.js

--- commands/staffjoin.js
+++ commands/staffjoin.js
@@ -11,32 +11,33 @@ module.exports = {
 		if(!message.client.staff.has(message.author.id)) {
 			return message.channel.send('no permission');
 		}
+		if(!args[0]) {
+			return message.channel.send('server name needed');
+		}
 		const sv = message.client.guilds.find(x=>x.name.toLowerCase().indexOf(args.join(' ').toLowerCase()) != -1);
-		if(sv) {
-			if(!sv.me.hasPermission('CREATE_INSTANT_INVITE') || !sv.me.hasPermission('ADMINISTRATOR')) {
-				return message.channel.send('insufficient permissions in the guild you want to join');
+		if(!sv) {
+			return message.author.send('could not find the desired server, either try a more/less precise search or it maybe just doesnt exist');
+		}
+		if(!sv.me.hasPermission('CREATE_INSTANT_INVITE') || !sv.me.hasPermission('ADMINISTRATOR')) {
+			return message.channel.send('insufficient permissions in the guild you want to join');
+		}
+		const ch = sv.channels.find(x=>x.name === 'irc');
+		if (ch) {
+			const bans = await sv.fetchBans();
+			if(bans.has(message.author.id)) {
+				await sv.unban(message.author.id);
 			}
-			const ch = sv.channels.find(x=>x.name === 'irc');
-			if (ch) {
-				const bans = await sv.fetchBans();
-				if(bans.has(message.author.id)) {
-					await sv.unban(message.author.id);
-				}
-				message.author.send('staff bypass');
-				ch.createInvite({ maxAge:0 }, 'someone requested to join this server')
-					.then((invite)=>{
-						message.author.send(`${sv.name}'s invite code:${invite.url}`);
-					})
-					.catch((err)=>{
-						console.log(err);
-					});
-			}
-			else{
-				return message.author.send('server has no #irc channel for me to get an invite from');
-			}
+			message.author.send('staff bypass');
+			ch.createInvite({ maxAge:0 }, 'someone requested to join this server')
+				.then((invite)=>{
+					message.author.send(`${sv.name}'s invite code:${invite.url}`);
+				})
+				.catch((err)=>{
+					console.log(err);
+				});
 		}
 		else{
-			return message.author.send('could not find the desired server, either try a more/less precise search or it maybe just doesnt exist');
+			return message.author.send('server has no #irc channel for me to get an invite from');
 		}
 
 

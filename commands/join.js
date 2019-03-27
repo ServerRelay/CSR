44906b5018592b3a4f83ffa959ce6646bcb226f1
DeltaRade <coolxpress26@gmail.com>
2019-03-27 17:53:31 -0400

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -67,7 +67,6 @@ module.exports = {
 
 			const filter = (reaction, user)=>(reaction.emoji.name === '✅' || reaction.emoji.name === '❎') && !user.bot;
 			const collector = await perm.awaitReactions(filter, { maxEmojis:1 });// {time:60000}
-			console.log(collector);
 			if(collector.has('❎')) {
 				// sv.owner.send('sending refusal')
 				message.author.send('denied permission');

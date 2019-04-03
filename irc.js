1b9eeb8937f6d8a0292acd26c7bb693f57c2380d
DeltaRade <coolxpress26@gmail.com>
2019-04-03 06:02:06 -0400

    Update irc.js

--- irc.js
+++ irc.js
@@ -356,34 +356,6 @@ function sendPrivate(message) {
 
 client.on('error', (err)=>{
 	console.log(err);
-	if (err.name == 'DiscordAPIError' && err.message == '401: Unauthorized') return process.exit();
-
-	if(err.name == 'DiscordAPIError') {
-		let addInfo = 'None Found!';
-		if(err.path !== undefined) {
-			const split = err.path.split('/');
-			const info = getDebugInfo(split);
-			addInfo = `Additional Debug Info:\n\tChannel: ${info.channel.name ? info.channel.name : 'Unknown'}\n\tGuild: ${info.channel.guild ? info.channel.guild.name : 'Unknown'}\n\tmessage content:${info.message ? info.message.cleanContent : 'None Found!'}`;
-		}
-
-		return (client.channels.get('543167247330312232')).send(`
-	\`\`\`js
-	Error: ${require('util').inspect(err).slice(0, 1800)}
-
-	${addInfo}
-		\`\`\`
-		`);
-	}
-
-
-	return (client.channels.get('543167247330312232')).send(`
-\`\`\`xs
-Client Error:
-Error: ${err.name}
-    ${err.message}
-    ${err.stack}
-    \`\`\`
-    `);
 });
 
 // unhandled rej ///////////////////////////////////////////////////////////////////////

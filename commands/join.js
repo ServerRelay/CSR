11d9a4cdf6ce60f9678d1171d934e9bb66803e5f
DeltaRade <coolxpress26@gmail.com>
2019-04-03 06:01:29 -0400

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -24,15 +24,30 @@ module.exports = {
 
 		const ed = new Discord.RichEmbed()
 			.setColor(rgbToHex(138, 0, 138));
+		const svs = [];
+		message.client.guilds.forEach(guild=>{
+			if(guild.name.toLowerCase().includes(args.join(' ').toLowerCase())) {
+				svs.push(guild);
+			}
+		});
+		if(!svs.length) return message.author.send('could not find the desired server, either try a more/less precise search or it maybe just doesnt exist');
+		const msg = svs.length > 1 ? await message.author.send(`this is a list of possible servers that were found:\`\`\`\n${svs.map((x, idx)=>idx + '. ' + x.name).join('\n')}\`\`\`\npleasy type the number that corresponds with the server to select it`, { split:true }) : await message.author.send('there was only 1 server found');
+		const filter = (m)=>!m.author.bot;
+		const collector = svs.length > 1 ? await msg.channel.awaitMessages(filter, { max:1, time:60000 }) : '';
+		const guild = svs.length > 1 ? svs[collector.first().content] : svs[0];
+		const authchannel = msg.channel;
+		if(!guild) {return authchannel.send('invalid index');}
+		if(guild.id == '495583899104182275') {
+			const ch = guild.channels.find(x=>x.name === 'irc');
+			const inv = await ch.createInvite('someone requested to join this server');
+			return await message.author.send(`request to join \`${guild.name}\` has been aproved\n${inv}`);
+		}
+		const ch = guild.channels.find(x=>x.name === 'irc');
+		if (!ch) return authchannel.send('Server has no #irc channel!');
 
-		const sv = message.client.guilds.get(args.join(' ')) || message.client.guilds.find(g => g.name === args.join(' ')) || message.client.guilds.find(x=>x.name.toLowerCase().indexOf(args.join(' ').toLowerCase()) != -1);
-		if(!sv) return message.author.send('could not find the desired server, either try a more/less precise search or it maybe just doesnt exist');
-
-		const ch = sv.channels.find(x=>x.name === 'irc');
-		if (!ch) return message.channel.send('Server has no #irc channel!');
-		message.author.send(`awaiting aproval from ${sv.name}...`);
-		const rq = await sv.createChannel('irc request', 'text', [{
-			id: sv.id,
+		message.author.send(`awaiting aproval from ${guild.name}...`);
+		const rq = await guild.createChannel('irc request', 'text', [{
+			id: guild.id,
 			deny: ['MANAGE_MESSAGES'],
 			allow: [],
 		},
@@ -42,14 +57,14 @@ module.exports = {
 			allow: 805829713,
 		}]
 		).catch(() => {
-			sv.owner.send(`Somebody tried to join ${sv.name} server but i could not create a #irc-requests channel!`).catch(()=>{
+			guild.owner.send(`Somebody tried to join ${guild.name} server but i could not create a #irc-requests channel!`).catch(()=>{
 				ch.send('Somebody tried to join this server but i could not create a #irc-requests channel!');
 			});
 		});
 		if(!rq) return;
 		try{
-			await rq.overwritePermissions(sv.id, { 'VIEW_CHANNEL':false });
-			sv.roles.forEach(async role => {
+			await rq.overwritePermissions(guild.id, { 'VIEW_CHANNEL':false });
+			guild.roles.forEach(async role => {
 				if(!role.hasPermission('KICK_MEMBERS')) {return;}
 				await rq.overwritePermissions(role.id, { 'VIEW_CHANNEL':true })
 					.catch(()=>{
@@ -59,25 +74,25 @@ module.exports = {
 
 			ed.setAuthor(`user ${message.author.username} is requesting an invite`, message.author.avatarURL || message.author.defaultAvatarURL);
 			ed.addField('Tag', `${message.author.tag}`, false);
-			ed.addField('server:', sv.name, false);
+			ed.addField('server:', guild.name, false);
 			// ed.addField('role in server',message.author.roles.first().name)
 			const perm = await rq.send(ed);
 			await perm.react('✅');
 			await perm.react('❎');
 
-			const filter = (reaction, user)=>(reaction.emoji.name === '✅' || reaction.emoji.name === '❎') && !user.bot;
-			const collector = await perm.awaitReactions(filter, { maxEmojis:1 });// {time:60000}
-			if(collector.has('❎')) {
+			const reqfilter = (reaction, user)=>(reaction.emoji.name === '✅' || reaction.emoji.name === '❎') && !user.bot;
+			const reqcollector = await perm.awaitReactions(reqfilter, { maxEmojis:1 });// {time:60000}
+			if(reqcollector.has('❎')) {
 				// sv.owner.send('sending refusal')
 				message.author.send('denied permission');
 				rq.delete();
 				return;
 			}
-			if(collector.has('✅')) {
+			if(reqcollector.has('✅')) {
 
 				ch.createInvite('someone requested to join this server')
 					.then((invite)=>{
-						message.author.send(`${sv.name}'s invite code:${invite.url}`);
+						message.author.send(`${guild.name}'s invite code:${invite.url}`);
 
 					})
 					.catch((err)=>{

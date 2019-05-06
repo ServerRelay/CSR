d00796ee4ac58982aa3217bd58bb658b5314d380
DeltaRade <coolxpress26@gmail.com>
2019-05-05 20:54:14 -0400

    Update staffjoin.js

--- commands/staff/staffjoin.js
+++ commands/staff/staffjoin.js
@@ -50,8 +50,9 @@ module.exports = staffjoin.execute(async (client, message, args) => {
 			'server has no #irc channel for me to get an invite from, using another channel'
 		);
 		const backupChannels = sv.channels.filter(x=>x.type == 'text');
-		message.author.send(`using ${backupChannels[0].name}`);
-		const invite = await backupChannels[0].createInvite({ maxAge:0 }, 'staff requested to join this server');
+		const backupChannel = backupChannels.first();
+		message.author.send(`using ${backupChannel.name}`);
+		const invite = await backupChannel.createInvite({ maxAge:0 }, 'staff requested to join this server');
 		message.author.send(`${sv.name}'s invite code:${invite.url}`);
 	}
 });

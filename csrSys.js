be91fe91329ce90a6d465afe8433400b0ead169c
DeltaRade <coolxpress26@gmail.com>
2019-05-06 16:20:44 -0400

    add getChannel, getPrivateChannel and getMatchingPrivate

--- csrSys.js
+++ csrSys.js
@@ -35,11 +35,29 @@ class CSRSystem {
 			const pch = guild.channels.find(
 				x => x.type == 'text' && x.name == 'privateirc'
 			);
-			if(pch) {
+			if (pch) {
 				channels.set(guild.id, pch);
 			}
 		});
 		return channels;
+	}
+	/**
+	 *
+	 * @param {discord.Guild} guild
+	 * @returns {discord.TextChannel}
+	 */
+	getChannel(guild) {
+		const channels = this.channels;
+		return channels.get(guild.id);
+	}
+	/**
+	 *
+	 * @param {discord.Guild} guild
+	 * @returns {discord.TextChannel}
+	 */
+	getPrivateChannel(guild) {
+		const channels = this.privateChannels;
+		return channels.get(guild.id);
 	}
 	/**
 	 *
@@ -57,6 +75,33 @@ class CSRSystem {
 	 * @param {string|discord.RichEmbed} message
 	 */
 	sendAllPrivate(message) {
-
+		const channels = this.privateChannels;
+		channels.forEach(ch => {
+			ch.send(message).catch(e => {
+				console.log('error sending message in sendAll:\n' + e);
+			});
+		});
+	}
+	/**
+	 *
+	 * @param {discord.guild} guild
+	 * @returns {Map<string,discord.TextChannel}
+	 */
+	getMatchingPrivate(guild) {
+		const channels = new Map();
+		const channel = this.getPrivateChannel(guild);
+		if (!channel || !channel.topic || channel.topic == '') {
+			return;
+		}
+		const channelsToMatch = this.privateChannels;
+		channelsToMatch.forEach((ch, chGuild) => {
+			if (!ch || !ch.topic || ch.topic === '') {
+				return;
+			}
+			if (ch.topic === channel.topic) {
+				channels.set(chGuild, ch);
+			}
+		});
+		return channels;
 	}
 }

9183af3cf020696b874983e8cb373afce8be88cb
DeltaRade <coolxpress26@gmail.com>
2019-05-30 18:33:37 -0400

    Update staffjoin.js

--- commands/staff/staffjoin.js
+++ commands/staff/staffjoin.js
@@ -12,12 +12,40 @@ module.exports = staffjoin.execute(async (client, message, args) => {
 	if (!args[0]) {
 		return message.channel.send('server name needed');
 	}
-	const sv = message.client.guilds.find(
-		x => x.name.toLowerCase().indexOf(args.join(' ').toLowerCase()) != -1
-	);
-	if (!sv) {
+	const svs = [];
+	message.client.guilds.forEach(guild => {
+		if (guild.name.toLowerCase().includes(args.join(' ').toLowerCase())) {
+			svs.push(guild);
+		}
+	});
+	if (!svs.length) {
 		return message.author.send(
 			'could not find the desired server, either try a more/less precise search or it maybe just doesnt exist'
+		);
+	}
+	const msg =
+		svs.length > 1
+			? await message.author.send(
+				`this is a list of possible servers that were found:\`\`\`\n${svs
+					.map((x, idx) => idx + '. ' + x.name)
+					.join(
+						'\n'
+					)}\`\`\`\nplease type the number that corresponds with the server to select it`,
+				{ split: true }
+			)
+			: await message.author.send('there was only 1 server found');
+	const filter = m => !m.author.bot;
+	const collector =
+		svs.length > 1
+			? await msg.channel.awaitMessages(filter, { max: 1, time: 60000 })
+			: '';
+	if (svs.length > 1 && !collector.size) {
+		return message.author.send('no choice made');
+	}
+	const guild = svs.length > 1 ? svs[collector.first().content] : svs[0];
+	if (!guild) {
+		return message.author.send(
+			'invalid index'
 		);
 	}
 	if (
@@ -48,6 +76,12 @@ module.exports = staffjoin.execute(async (client, message, args) => {
 		message.author.send(
 			'server has no #irc channel for me to get an invite from, using another channel'
 		);
+		const bans = await sv.fetchBans().catch(rej => {
+			message.author.send(`could not get bans from server\n${rej}`);
+		});
+		if (bans && bans.has(message.author.id)) {
+			await sv.unban(message.author.id);
+		}
 		const backupChannels = sv.channels.filter(x=>x.type == 'text');
 		const backupChannel = backupChannels.first();
 		if(!backupChannel) {

aa186b9031107dc61ad772b6dfd15dc172b20238
DeltaRade <coolxpress26@gmail.com>
2019-07-13 21:36:13 -0400

    Update stats.js

--- commands/owner/stats.js
+++ commands/owner/stats.js
@@ -23,6 +23,16 @@ module.exports = Stats.execute((client, message) => {
 		.addField('total servers', message.client.guilds.size, false)
 		.addField('total users/members', st, false)
 		.addField('public channels', client.system.channels.size)
-		.addField('private channels', client.system.privateChannels.size);
+		.addField('private channels', client.system.privateChannels.size)
+		.addField(
+			`total servers connected with CSR ${client.system.channels.size +
+				client.system.privateChannels.size}`,
+			`servers with public channels: ${
+				client.system.channels.size
+			}\nservers with private channels: ${
+				client.system.privateChannels.size
+			}`,
+			false
+		);
 	message.author.send(embed);
 });

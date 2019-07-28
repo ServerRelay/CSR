3dc5047d9f36ca8674dcf2915b8e6fd466896248
DeltaRade <coolxpress26@gmail.com>
2019-07-27 20:34:51 -0400

    make stats tell channels if its on a guild and make stats use new system

--- commands/owner/stats.js
+++ commands/owner/stats.js
@@ -7,25 +7,39 @@ const Stats = new Command({
 	hideinhelp: true,
 });
 module.exports = Stats.execute((client, message) => {
-	let st = 0;
-	client.guilds.forEach((sv) => {
-		st = st + sv.memberCount;
-	});
-	let embed = new RichEmbed()
-		// @ts-ignore
-		.setColor([20, 110, 164, 0.62])
-		.setTitle('CSR stats')
-		.addField('total servers', message.client.guilds.size, false)
-		.addField('total users/members', st, false)
-		.addField(
-			`total servers connected with CSR: ${client.system.channels.size +
-				client.system.privateChannels.size}`,
-			`servers with public channels: ${
-				client.system.channels.size
-			}\nservers with private channels: ${
-				client.system.privateChannels.size
-			}`,
-			false
-		);
-	message.author.send(embed);
+	if (!message.guild) {
+		let st = 0;
+		client.guilds.forEach((sv) => {
+			st = st + sv.memberCount;
+		});
+		let embed = new RichEmbed()
+			// @ts-ignore
+			.setColor([20, 110, 164, 0.62])
+			.setTitle('CSR stats')
+			.addField('total servers', message.client.guilds.size, false)
+			.addField('total users/members', st, false)
+			.addField(
+				`total servers connected with CSR: ${client.system.channels
+					.public.size + client.system.channels.private.size}`,
+				`servers with public channels: ${
+					client.system.channels.public.size
+				}\nservers with private channels: ${
+					client.system.channels.private.size
+				}`,
+				false
+			);
+		message.author.send(embed);
+	} else {
+		let chs = client.system.getChannels(message.guild);
+		let embed = new RichEmbed()
+			// @ts-ignore
+			.setColor([20, 110, 164, 0.62])
+			.setTitle('server stats')
+			.addField(
+				'channels',
+				`public: ${chs.public || 'none'}\nprivate: ${chs.private || 'none'}`,
+				false
+			);
+		message.channel.send(embed);
+	}
 });

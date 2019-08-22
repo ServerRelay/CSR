3cf0338741a84eea94bc0cf371dbf22befb4c363
DeltaRade <coolxpress26@gmail.com>
2019-08-22 18:22:54 -0400

    Create support.js

---
+++ commands/support.js
@@ -0,0 +1,14 @@
+const { Command } = require('easy-djs-commandhandler');
+const discord = require('discord.js');
+module.exports = new Command({
+	name: 'support',
+	description:
+		'Do you have an issue/suggestion or bot is having problems? you can join the support server and ask',
+	requiresBotPermissions: ['EMBED_LINKS'],
+}).execute((client, message, args) => {
+	let embed = new discord.RichEmbed();
+	embed.setColor(client.color);
+	embed.setTitle('have an issue? bot nor working?');
+	embed.setDescription('join the support server\nhttps://discord.gg/GTkjQFr');
+	message.channel.send(embed);
+});

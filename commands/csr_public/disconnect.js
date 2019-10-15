44207104a2d55656d195aa2648abc7b01ae8f4c4
DeltaRade <coolxpress26@gmail.com>
2019-10-15 15:02:57 -0400

    delete webhooks on disconnect

--- commands/csr_public/disconnect.js
+++ commands/csr_public/disconnect.js
@@ -2,7 +2,7 @@ const jndb = require('jndb');
 const { Command } = require('easy-djs-commandhandler');
 const Disconnect = new Command({
 	name: 'disconnect',
-	requires:['guild'],
+	requires: ['guild'],
 	requiresBotPermissions: ['EMBED_LINKS'],
 	description: 'disconnects channel from csr',
 	usage: '<prefix>disconnect [public | private]',
@@ -25,10 +25,12 @@ module.exports = Disconnect.execute((client, message, args) => {
 		);
 	}
 	if (type == 'public') {
-		client.system.channels.delete(message.guild,'public')
+		client.system.channels.delete(message.guild, 'public');
+		client.system.webhookManager.webhooks.public.delete(message.guild.id);
 	} else {
-		client.system.channels.delete(message.guild,'private')
+		client.system.channels.delete(message.guild, 'private');
+		client.system.webhookManager.webhooks.private.delete(message.guild.id);
 	}
-	
+
 	message.channel.send('successfully disconnected');
 });

d05feea03c2c5eab43c39b54394b8735d79b813c
DeltaRade <46230290+DeltaRade@users.noreply.github.com>
2019-11-27 14:37:41 -0400

    create testwebhooks command

---
+++ commands/owner/testWebhooks.js
@@ -0,0 +1,40 @@
+const { Command } = require('easy-djs-commandhandler');
+module.exports = new Command({
+	name: 'testwebhooks',
+	description: '',
+	aliases: [],
+	requires: ['guild', 'botowner'],
+	requiresBotPermissions: [],
+}).execute(async (client, message, args) => {
+	const guild = message.guild;
+	message.author = client;
+	message.content = 'test';
+	let failed = false;
+	let failReason = '';
+	let webhooks = client.system.webhookManager.get(guild);
+	if (!webhooks.public && !webhooks.private) {
+		message.channel.send(
+			'❌ failed: could not find any webhooks belonging to this guild'
+		);
+		return;
+	}
+	if (webhooks.public) {
+		await client.system.webhookManager
+			.send(webhooks.public, message)
+			.catch((err) => {
+				(failed = true), (failReason = err);
+			});
+	}
+	if (webhooks.private && !failed) {
+		await client.system.webhookManager
+			.send(webhooks.private, message)
+			.catch((err) => {
+				(failed = true), (failReason = err);
+			});
+	}
+	if (failed) {
+		message.channel.send('❌ failed: ' + failReason);
+		return;
+	}
+	message.channel.send('✅ success: everything seems to be working');
+});
